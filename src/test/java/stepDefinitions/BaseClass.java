403f9a21dfbb754d8c2756f538425b43c401d351
unknown <vijirv@yahoo.com>
2020-05-21 00:15:18 +0100

    modified base class

--- src/test/java/stepDefinitions/BaseClass.java
+++ src/test/java/stepDefinitions/BaseClass.java
@@ -23,38 +23,7 @@ public class BaseClass {
     public AddCustomerPage addCustomerPage;
     public SearchCustomerPage searchCustomerPage;
     public static Logger logger;
-/*
-    public WebDriver init_driver(Properties prop) {
 
-        String browserName = prop.getProperty("browser");
-        System.out.println("Running on ----> " + browserName + " browser");
-
-        if (browserName.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        } else if (browserName.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-        } else {
-            System.out.println(browserName + " is not found, please pass the right browser Name");
-        }
-        return driver;
-    }
-
-    public Properties init_properties(){
-        prop = new Properties();
-        String path = "C:\\Users\\Viji\\IdeaProjects\\SeleniumMavenBDD\\src\\test\\java\\config\\config.properties";
-        try {
-            FileInputStream ip = new FileInputStream(path);
-            prop.load(ip);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return  prop;
-    }
-*/
     //Created for generating random string for email
     public static String randomestring() {
         String generatedString1 =  RandomStringUtils.randomAlphabetic(5);
